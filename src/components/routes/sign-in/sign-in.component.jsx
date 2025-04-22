ebe6be0737e82d318284289e2b8d17b8447a49a8
jasleen.kaur94620@gmail.com <jasleen.kaur94620@gmail.com>
2025-04-22 02:06:12 -0400

    added signupform, form-input and button component

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import {
   signInWithGooglePopup,
 } from "../../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -15,6 +16,8 @@ const SignIn = () => {
       <h1>Sign In</h1>
       <p>Please sign in to access your account.</p>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      <SignUpForm />
+      <p>Don't have an account? Sign up now!</p>
     </div>
   );
 };
---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import FormInput from "../form-input/form-input.component";
+import { Form } from "react-router-dom";
+import "./sign-up-form.styles.scss";
+import Button from "../button/button.component";
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { displayName, email, password, confirmPassword } = formData;
+
+  const resetFormFields = () => {
+    setFormData({
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    try {
+      // Add sign-up logic here
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+      // Optionally, you can redirect the user or show a success message
+      console.log("User signed up successfully:", user);
+      console.log("User signed up with email:", email);
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Email already in use");
+      }
+
+      console.error("Error signing up:", error);
+    }
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Don't have and account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          label="Email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+          required
+        />
+        <Button buttonType="default" type="submit">
+          Sign Up
+        </Button>
+      </form>
+    </div>
+  );
+};
+export default SignUpForm;
